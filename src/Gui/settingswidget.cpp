8f93577d37e3d1619464a91f8c67f2dd04474a81
Alexandre Moore <alexandre.moore@kernelcoffee.org>
2012-11-21 00:02:09 -0500

    update settings panel

--- src/Gui/settingswidget.cpp
+++ src/Gui/settingswidget.cpp
@@ -1,12 +1,24 @@
 #include "settingswidget.h"
 #include "Utilities/logger.h"
 
+#include <QTabWidget>
+#include <QDialogButtonBox>
+
 SettingsWidget::SettingsWidget(QWidget *parent) :
     QDialog(parent)
 {
     Logger::log("init SettingsWidget", LOG_DEBUG);
 
     setupUi(this);
+//    QTabWidget *tabView = new QTabWidget();
+//    tabView->addTab()
+
+//    QDialogButtonBox *buttonBox = new QDialogButtonBox();
+//    buttonBox->setOrientation(Qt::Horizontal);
+//    buttonBox->setStandardButtons(QDialogButtonBox::Apply|QDialogButtonBox::Cancel|QDialogButtonBox::Ok|QDialogButtonBox::Reset);
+
+//    layout()->addWidget(tabViewb);
+//    layout()->addWidget(buttonBox);
 }
 
 SettingsWidget::~SettingsWidget()
