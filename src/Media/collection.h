e3bf0de209396a2b9c3bb43fe149e9d61da5c543
Alexandre Moore <amoore@ics.com>
2012-11-26 09:41:58 -0500

    update collection

--- src/Media/collection.h
+++ src/Media/collection.h
@@ -8,19 +8,22 @@
 #include "album.h"
 #include "artist.h"
 #include "playlist.h"
-
+#include "watchplaylist.h"
 
 class Collection
 {
 public:
     Collection();
     ~Collection();
+public slots:
+    void    setWatchFolder(QString);
 protected:
     QMap<int, Track*>   *m_tracks;
     QMap<int, Artist*>  *m_artists;
     QMap<int, Album*>   *m_albums;
 
     QVector<Playlist*>  *m_playlists;
+    WatchPlaylist       *m_watchPlaylist;
 };
 
 #endif // COLLECTION_H
